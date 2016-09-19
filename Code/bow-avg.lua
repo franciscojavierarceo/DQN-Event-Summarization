76d0c671f63901b0a67fc9a07c44793bbae09b97
Francisco Arceo <arceofrancisco@gmail.com>
2016-09-19 11:45:14 -0400

    Updated bow-avg example

--- Code/bow-avg.lua
+++ Code/bow-avg.lua
@@ -4,7 +4,6 @@ require 'rnn'
 vocab_size = 4
 embeddings_dim = 3
 
---- Example 1
 print("**********************************************************************")
 print("                                 Example 1                            ")
 print("**********************************************************************")
@@ -14,7 +13,7 @@ print("**********************************************************************")
 --   we pad it with zeros.
 
 input = torch.LongTensor{
-                        {1, 4, 2, 1}, 
+                        {1, 4, 2, 0}, 
                         {1, 3, 3, 2}
                      }
 print("Format of data in Example 1 is")
@@ -22,6 +21,7 @@ print(input)
 
 --   Here we add the look up table which will produce output of size
 --      (batch size x max sequence length x embedding dim size)
+
 --   In this example ouptut O[1][4] is a vector of zeros because 
 --   of the zero pad.
 
@@ -40,65 +40,38 @@ print(#net:forward(input))
 -- flag is true. This is more or less what we want but there is an 
 -- issue with the averaging part of the sum layer. 
 
--- What is it? (You can ignore it for now and through the output of this
--- into a mlp. We can discuss how to fix it on Tuesday.)
-
----net:add(nn.Sum(2, 3, true))
----print("Dimensions after lookup table layer and averaging layer:")
----print(#net:forward(input))
+-- What is it? (You can ignore it for now and throw the output of this
+-- into an mlp. We can discuss how to fix it on Tuesday.)
 
 net:add(nn.Sum(2, 3, true))
 print("Dimensions after lookup table layer and averaging layer:")
 print(net:forward(input:nonzero()))
-print('Original...')
+print('Original nnsum...')
 print(#net:forward(input))
-print('Nonzero...')
+print('Nonzero nnsum...')
 print(#net:forward(input:nonzero()))
 
-
-
---- Example 2
 print("**********************************************************************")
 print("                                 Example 2                            ")
 print("**********************************************************************")
+
 input = torch.LongTensor{
-                        {1, 4, 2, 0}, 
+                        {1, 4, 2, 1}, 
                         {1, 3, 3, 2}
                      }
 print("Format of data in Example 2 is")
 print(input)
 
---   Here we add the look up table which will produce output of size
---      (batch size x max sequence length x embedding dim size)
---   In this example ouptut O[1][4] is a vector of zeros because 
---   of the zero pad.
-
 net = nn.Sequential()
 net:add(nn.LookupTableMaskZero(vocab_size, embeddings_dim))
 print("Dimensions of lookup table layer:")
 print(#net:forward(input))
 
--- Now add the summation layer. The arguments are which dimension to
--- sum over, the total number of input dimensions, and a boolean flag
--- indicating whether or not to average the sum.
-
--- We are summing over the sequence which corresponds to dimension 2.
--- The input has 3 dimensions (output of lookup layer is 
--- batch x sequence x embedding) and we want to average the sum so 
--- flag is true. This is more or less what we want but there is an 
--- issue with the averaging part of the sum layer. 
-
--- What is it? (You can ignore it for now and through the output of this
--- into a mlp. We can discuss how to fix it on Tuesday.)
-
----net:add(nn.Sum(2, 3, true))
----print("Dimensions after lookup table layer and averaging layer:")
----print(#net:forward(input))
-
 net:add(nn.Sum(2, 3, true))
+
 print("Dimensions after lookup table layer and averaging layer:")
 print(net:forward(input:nonzero()))
-print('Original...')
+print('Original nnsum...')
 print(#net:forward(input))
-print('Nonzero...')
+print('Nonzero nnsum...')
 print(#net:forward(input:nonzero()))
