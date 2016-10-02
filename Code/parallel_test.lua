c6e9957dfae79d7a93df6d3d99550dceb9df9c4e
Francisco Arceo <arceofrancisco@gmail.com>
2016-10-02 16:50:32 -0400

    Updated code now have the full model architecture working on a simple example---need to implement for our actual model

--- Code/parallel_test.lua
+++ Code/parallel_test.lua
@@ -1,80 +1,53 @@
+require 'nn'
 require 'rnn'
 
--- mlp = nn.Sequential()       -- Create a network that takes a Tensor as input
--- mlp:add(nn.SplitTable(2))
--- c = nn.ParallelTable()      -- The two Tensor slices go through two different Linear
--- c:add(nn.Linear(10, 3))     -- Layers in Parallel
--- c:add(nn.Linear(5, 7))
--- mlp:add(c)                  -- Outputing a table with 2 elements
--- p = nn.ParallelTable()      -- These tables go through two more linear layers separately
--- p:add(nn.Linear(3, 2))
--- p:add(nn.Linear(7, 1))
--- mlp:add(p)
--- mlp:add(nn.JoinTable(1))    -- Finally, the tables are joined together and output.
+function buildLSTM(vsize, edim, odim)
+    local lstm = nn.Sequential()
+    lstm:add(nn.LookupTableMaskZero(vsize, edim))
+    lstm:add(nn.SplitTable(1, edim))
+    lstm:add(nn.Sequencer(nn.LSTM(edim, edim)))
+    lstm:add(nn.SelectTable(-1))
+    return lstm
+end
 
+sentences = torch.LongTensor{{0, 1, 3, 4}, {2, 1, 4, 3}}:t()
+summary = torch.LongTensor{{0, 0, 1, 4}, {0, 2, 3, 1}}:t()
+query = torch.LongTensor{{0, 0, 4, 3}, {0, 1, 3, 2}}:t()
+actions = torch.round(torch.rand(2, 1))
+yrouge = torch.rand(2, 1)
 
-xs = torch.LongTensor{{0, 1, 3, 4}, {2, 1, 4, 3}}
-x = xs:t()
-y = torch.randn(10)
-z = torch.rand(5)
-
+batch_size = 2
 vocab_size = 4
-embed_dim = 3
+embed_dim = 10
 outputSize = 1
+learning_rate = 0.1
 
-lstm = nn.Sequential()
-lstm:add(nn.LookupTableMaskZero(vocab_size, embed_dim)) -- will return a sequence-length x batch-size x embedDim tensor
-lstm:add(nn.SplitTable(1, embed_dim)) -- splits into a sequence-length table with batch-size x embedDim entries
-lstm:add(nn.Sequencer(nn.LSTM(embed_dim, embed_dim)))
-lstm:add(nn.SelectTable(-1)) -- selects last state of the LSTM
-lstm:add(nn.Linear(embed_dim, outputSize)) -- map last state to a score for classification
+lstm1 = buildLSTM(vocab_size, embed_dim, outputSize)
+lstm2 = buildLSTM(vocab_size, embed_dim, outputSize)
+lstm3 = buildLSTM(vocab_size, embed_dim, outputSize)
 
-p = nn.ParallelTable()
-p:add(nn.Linear(10, 2))
-p:add(nn.Linear(5, 3))
--- lstm:add(p)
+mlp1 = nn.Sequential()
+mlp1:add(nn.Linear(1, embed_dim))
 
-print(lstm:forward(x))
-print(p:forward{y, z})
+ParallelModel = nn.ParallelTable()
+ParallelModel:add(lstm1)
+ParallelModel:add(lstm2)
+ParallelModel:add(lstm3)
+ParallelModel:add(mlp1)
 
-lstm2 = nn.Sequential()
-lstm2:add(nn.LookupTableMaskZero(vocab_size, embed_dim)) -- will return a sequence-length x batch-size x embedDim tensor
-lstm2:add(nn.SplitTable(1, embed_dim)) -- splits into a sequence-length table with batch-size x embedDim entries
-lstm2:add(nn.Sequencer(nn.LSTM(embed_dim, embed_dim)))
-lstm2:add(nn.SelectTable(-1)) -- selects last state of the LSTM
-lstm2:add(nn.Linear(embed_dim, outputSize)) -- map last state to a score for classification
+FinalMLP = nn.Sequential()
+FinalMLP:add(ParallelModel)
+FinalMLP:add(nn.JoinTable(2))
+FinalMLP:add( nn.Linear(embed_dim * 4, 1) )
 
-g = nn.ParallelTable()
-g:add(nn.Linear(10, 2))
-g:add(nn.Linear(5, 3))
-lstm2:add(g)
+criterion = nn.MSECriterion()
 
--- print(lstm2:forward{x,y,z})
--- print(lstm2:forward(x,y,z))
-mlp = nn.Sequential()         -- Create a network that takes a Tensor as input
-c = nn.ConcatTable()          -- The same Tensor goes through two different Linear
-c:add(nn.Linear(10, 3))       -- Layers in Parallel
-c:add(nn.Linear(10, 7))
-mlp:add(c)                    -- Outputing a table with 2 elements
-p = nn.ParallelTable()        -- These tables go through two more linear layers
-p:add(nn.Linear(3, 2))        -- separately.
-p:add(nn.Linear(7, 1))
-mlp:add(p)
-mlp:add(nn.JoinTable(1))      -- Finally, the tables are joined together and output.
-
-print(torch.randn(10))
-pred = mlp:forward(torch.randn(10))
-print(pred)
-
--- pred = lstm:forward(x, y, z)
--- pred = lstm:forward{x, y, z}
-
--- y:copy(x:select(2, 1):narrow(1, 1, 3))
--- pred = mlp:forward(x)
-
--- criterion = nn.MSECriterion()
--- err = criterion:forward(pred, y)
--- gradCriterion = criterion:backward(pred, y)
--- mlp:zeroGradParameters()
--- mlp:backward(x, gradCriterion)
--- mlp:updateParameters(0.05)
+for epoch=1, 100 do
+    preds = FinalMLP:forward({sentences, summary, query, actions})
+    loss = criterion:forward(preds, yrouge)
+    grads = criterion:backward(preds, yrouge)
+    FinalMLP:backward({sentences, summary, query, actions}, grads)
+    FinalMLP:updateParameters(learning_rate)
+    FinalMLP:zeroGradParameters()
+    print(string.format("Epoch %i, loss =%6f", epoch, loss))
+end
