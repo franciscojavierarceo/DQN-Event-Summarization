953ed482d39e69638589a11ef3a88bb7b710c0f4
Francisco Arceo <arceofrancisco@gmail.com>
2016-10-11 10:25:19 -0400

    updated run_models.sh to execute the new torch script

--- Code/run_models.sh
+++ Code/run_models.sh
@@ -4,8 +4,8 @@
 # th ./dqn_random.lua
 
 echo "DQN BOW"
-th ./run_model_all.lua --model bow  > dqn_full_bow_model_perf.txt
+th ./dqn_model.lua --model bow  > dqn_full_bow_model_perf.txt
 
 echo "DQN LSTM"
-th ./run_model_all.lua --model lstm  > dqn_full_lstm_model_perf.txt
+th ./dqn_model.lua --model lstm  > dqn_full_lstm_model_perf.txt
 echo "End"
