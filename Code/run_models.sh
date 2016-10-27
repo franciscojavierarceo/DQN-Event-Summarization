0718f595f3f268b37b07c0fc21be17f49ccf9584
Francisco Arceo <Francisco.Arceo@cba.com.au>
2016-10-27 01:00:57 -0400

    Updated script to run models

--- Code/run_models.sh
+++ Code/run_models.sh
@@ -1,14 +1,14 @@
 # /bin/sh
 
 echo "DQN BOW"
-th ./dqn_model.lua --model bow  > Performance/dqn_full_bow_model_perf.txt
+th ./dqn_model.lua --nn_model bow  > Performance/dqn_full_bow_model_perf.txt
 
 # These are to clear up the memory
 pkill th
 pkill luajit
 
 echo "DQN LSTM"
-th ./dqn_model.lua --model lstm  > Performance/dqn_full_lstm_model_perf.txt
+th ./dqn_model.lua --nn_model lstm  > Performance/dqn_full_lstm_model_perf.txt
 echo "End"
 
 pkill th
@@ -16,13 +16,13 @@ pkill luajit
 
 echo "DQN BOW - skip_rate = 0.25"
 
-th ./dqn_model.lua --model bow  --skip_rate 0.25 > Performance/dqn_full_bow_model_perf_sr025.txt
+th ./dqn_model.lua --nn_model bow  --skip_rate 0.25 > Performance/dqn_full_bow_model_perf_sr025.txt
 
 pkill th
 pkill luajit
 
 echo "DQN LSTM - skip_rate = 0.25"
-th ./dqn_model.lua --model lstm  --skip_rate 0.25 > Performance/dqn_full_lstm_model_perf_sr025.txt
+th ./dqn_model.lua --nn_model lstm  --skip_rate 0.25 > Performance/dqn_full_lstm_model_perf_sr025.txt
 echo "End"
 
 
@@ -30,11 +30,11 @@ pkill th
 pkill luajit
 
 echo "DQN BOW - skip_rate = 0.75"
-th ./dqn_model.lua --model bow  --skip_rate 0.75 > Performance/dqn_full_bow_model_perf_sr075.txt
+th ./dqn_model.lua --nn_model bow  --skip_rate 0.75 > Performance/dqn_full_bow_model_perf_sr075.txt
 
 pkill th
 pkill luajit
 
 echo "DQN LSTM - skip_rate = 0.75"
-th ./dqn_model.lua --model lstm  --skip_rate 0.75 > Performance/dqn_full_lstm_model_perf_sr075.txt
+th ./dqn_model.lua --nn_model lstm  --skip_rate 0.75 > Performance/dqn_full_lstm_model_perf_sr075.txt
 echo "End"
