c72e1336403efbbaf8a138559ffa2da806c5e824
Francisco Arceo <Francisco.Arceo@cba.com.au>
2016-11-28 09:59:55 -0500

    Well, I screwed it up again...its close though

--- Code/model_utils2.lua
+++ Code/model_utils2.lua
@@ -1,64 +1,3 @@
-function intialize_variables(query_file, inputs, input_path, K_tokens, maxSummarySize)
-    local vocabSize = 0
-    local maxseqlen = 0
-    local maxseqlenq = getMaxseq(query_file)
-    local vocabSizeq = getVocabSize(query_file)
-    local query_data = {}
-    for query_id = 1, #inputs do
-        input_fn = inputs[query_id]['inputs']
-        nugget_fn = inputs[query_id]['nuggets']
-
-        input_file = csvigo.load({path = input_path .. input_fn, mode = "large", verbose = false})
-        nugget_file = csvigo.load({path = input_path .. nugget_fn, mode = "large", verbose = false})
-        -- input_file = geti_n(input_file, 2, #input_file) 
-        input_file = geti_n(input_file, 2, n) 
-        nugget_file = geti_n(nugget_file, 2, #nugget_file) 
-
-        vocabSize = math.max(vocabSize, vocabSizeq, getVocabSize(input_file))
-        maxseqlen = math.max(maxseqlen, maxseqlenq, getMaxseq(input_file))
-        xtdm  = buildTermDocumentTable(input_file, K_tokens)
-        nuggets = buildTermDocumentTable(nugget_file, nil)
-        ntdm = {}
-        for i=1, #nuggets do
-            ntdm = tableConcat(table.unpack(nuggets), ntdm)
-        end
-        -- Initializing the bookkeeping variables and storing them
-        local query = LongTensor{inputs[query_id]['query'] }
-        local sentenceStream = LongTensor(padZeros(xtdm, K_tokens))
-        local streamSize = sentenceStream:size(1)
-        local refSummary = Tensor{ntdm}
-        local refCounts = buildTokenCounts(refSummary)
-        local buffer = Tensor(1, maxSummarySize):zero()
-        local actions = ByteTensor(streamSize, 2):fill(0)
-        local actionsOpt = ByteTensor(streamSize, 2):fill(0)
-        local exploreDraws = Tensor(streamSize)
-        local summaryBuffer = LongTensor(streamSize + 1, maxSummarySize):zero()
-        local qValues = Tensor(streamSize, 2):zero()
-        local rouge = Tensor(streamSize + 1):zero()
-        local rougeOpt = Tensor(streamSize + 1):zero()
-        local summary = summaryBuffer:zero():narrow(1,1,1)
-        local oracleF1, oracleActions = scoreOracle(sentenceStream, maxSummarySize, refCounts)
-
-        query_data[query_id] = {
-            sentenceStream,
-            streamSize,
-            query,
-            actions,
-            exploreDraws,
-            summaryBuffer,
-            qValues,
-            rouge,
-            actionsOpt,
-            rougeOpt,
-            refSummary,
-            refCounts,
-            buffer,
-            oracleF1
-        }
-    end
-    return vocabSize, query_data
-end
-
 function scoreOracle(sentenceStream, maxSummarySize, refCounts)
     local buffer = Tensor(1, maxSummarySize):zero()
     local streamSize = sentenceStream:size(1)
@@ -92,7 +31,7 @@ function buildModel(model, vocabSize, embeddingSize, use_cuda)
         print(string.format("Running bag-of-words model to learn %s", metric))
         sentenceLookup = nn.Sequential()
                     :add(nn.LookupTableMaskZero(vocabSize, embeddingSize))
-                    -- Not averaging really blows up the model so keep this true
+                    -- Note not averaging really blows up the model so keep this true
                     :add(nn.Sum(2, 3, true))
                     :add(nn.ReLU())
                     -- :add(nn.Tanh())
@@ -292,7 +231,33 @@ function backProp(input_memory, params, gradParams, optimParams, model, criterio
     end
     return loss/n_backprops
 end
-function backPropOld(input_memory, params, model, criterion, batch_size, memsize, use_cuda)
+-- function backPropOld(input_memory, params, model, criterion, batch_size, memsize, use_cuda)
+--     local inputs = {input_memory[1], input_memory[3]}
+--     local rewards = input_memory[2]
+--     local dataloader = dl.TensorLoader(inputs, rewards)
+--     for k, xin, reward in dataloader:sampleiter(batch_size, memsize) do
+--         xinput = xin[1]
+--         actions_in = xin[2]
+--         local function feval(params)
+--             gradParams:zero()
+--             local maskLayer = nn.MaskedSelect()
+--             if use_cuda then 
+--              maskLayer = maskLayer:cuda()
+--             end
+--             local predQ = model:forward(xinput)
+--             local predQOnActions = maskLayer:forward({predQ, actions_in}) 
+--             local lossf = criterion:forward(predQOnActions, reward)
+--             local gradOutput = criterion:backward(predQOnActions, reward)
+--             local gradMaskLayer = maskLayer:backward({predQ, actions_in}, gradOutput)
+--             model:backward(xinput, gradMaskLayer[1])
+--             return lossf, gradParams
+--         end
+--      --- optim.rmsprop returns \theta, f(\theta):= loss function
+--      _, lossv  = optim.rmsprop(feval, params, optimParams)   
+--     end
+--     return lossv[1]
+-- end
+function backPropOld(input_memory, params, gradParams, optimParams, model, criterion, batch_size, memsize, use_cuda)
     local inputs = {input_memory[1], input_memory[3]}
     local rewards = input_memory[2]
     local dataloader = dl.TensorLoader(inputs, rewards)
@@ -317,4 +282,234 @@ function backPropOld(input_memory, params, model, criterion, batch_size, memsize
      _, lossv  = optim.rmsprop(feval, params, optimParams)   
     end
     return lossv[1]
+end
+function intialize_variables(query_file, inputs, n_samples, input_path, K_tokens, maxSummarySize)
+    local vocabSize = 0
+    local maxseqlen = 0
+    local maxseqlenq = getMaxseq(query_file)
+    local vocabSizeq = getVocabSize(query_file)
+    local query_data = {}
+    for query_id = 1, #inputs do
+        input_fn = inputs[query_id]['inputs']
+        nugget_fn = inputs[query_id]['nuggets']
+
+        input_file = csvigo.load({path = input_path .. input_fn, mode = "large", verbose = false})
+        nugget_file = csvigo.load({path = input_path .. nugget_fn, mode = "large", verbose = false})
+        -- This is just for experimentation
+        input_file = geti_n(input_file, 2,  n_samples) 
+        -- input_file = geti_n(input_file, 2, #input_file) 
+        nugget_file = geti_n(nugget_file, 2, #nugget_file) 
+
+        vocabSize = math.max(vocabSize, vocabSizeq, getVocabSize(input_file))
+        maxseqlen = math.max(maxseqlen, maxseqlenq, getMaxseq(input_file))
+        xtdm  = buildTermDocumentTable(input_file, K_tokens)
+        nuggets = buildTermDocumentTable(nugget_file, nil)
+        ntdm = {}
+        for i=1, #nuggets do
+            ntdm = tableConcat(table.unpack(nuggets), ntdm)
+        end
+        -- Initializing the bookkeeping variables and storing them
+        local query = LongTensor{inputs[query_id]['query'] }
+        local sentenceStream = LongTensor(padZeros(xtdm, K_tokens))
+        local streamSize = sentenceStream:size(1)
+        local refSummary = Tensor{ntdm}
+        local refCounts = buildTokenCounts(refSummary)
+        local buffer = Tensor(1, maxSummarySize):zero()
+        local actions = ByteTensor(streamSize, 2):fill(0)
+        local actionsOpt = ByteTensor(streamSize, 2):fill(0)
+        local exploreDraws = Tensor(streamSize)
+        local summaryBuffer = LongTensor(streamSize + 1, maxSummarySize):zero()
+        local qValues = Tensor(streamSize, 2):zero()
+        local rouge = Tensor(streamSize + 1):zero()
+        local rougeOpt = Tensor(streamSize + 1):zero()
+        local summary = summaryBuffer:zero():narrow(1,1,1)
+        local oracleF1, oracleActions = scoreOracle(sentenceStream, maxSummarySize, refCounts)
+
+        query_data[query_id] = {
+            sentenceStream,
+            streamSize,
+            query,
+            actions,
+            exploreDraws,
+            summaryBuffer,
+            qValues,
+            rouge,
+            actionsOpt,
+            rougeOpt,
+            refSummary,
+            refCounts,
+            buffer,
+            oracleF1
+        }
+    end
+    return vocabSize, query_data
+end
+
+function train(inputs, query_data, model, nepochs, nnmod, metric, thresh, gamma, epsilon, delta, base_explore_rate, end_baserate, mem_size, batch_size, optimParams, n_backprops, use_cuda)
+    math.randomseed(420)
+    torch.manualSeed(420)
+    criterion = nn.MSECriterion()
+
+    if use_cuda then
+        criterion = criterion:cuda()
+        model = model:cuda()
+    end
+
+    local params, gradParams = model:getParameters()
+    local perf = io.open(string.format("%s_perf.txt", nnmod), 'w')
+    for epoch=0, nepochs do
+        for query_id=1, #inputs do
+            -- Initializing local variables
+                -- in principal I could use the query_data[query_id][1] 
+                -- as the argument but that's much less readable
+            local sentenceStream = query_data[query_id][1]
+            local streamSize = query_data[query_id][2]
+            local query = query_data[query_id][3]
+            local actions = query_data[query_id][4]
+            local exploreDraws = query_data[query_id][5]
+            local summaryBuffer = query_data[query_id][6]
+            local qValues = query_data[query_id][7]
+            local rouge = query_data[query_id][8]
+            local actionsOpt = query_data[query_id][9]
+            local rougeOpt = query_data[query_id][10]
+            local refSummary = query_data[query_id][11]
+            local refCounts = query_data[query_id][12]
+            local buffer = query_data[query_id][13]
+            local oracleF1 = query_data[query_id][14]
+            exploreDraws:uniform(0, 1)
+
+            for i=1, streamSize do      -- Iterating through individual sentences
+                local sentence = sentenceStream:narrow(1, i, 1)
+                qValues[i]:copy(model:forward({sentence, query, summary}))
+
+                -- epsilon greedy strategy
+                if exploreDraws[i]  <=  epsilon then        
+                    actions[i][torch.random(SKIP, SELECT)] = 1
+                else 
+                    if qValues[i][SKIP] > qValues[i][SELECT] then
+                        actions[i][SKIP] = 1
+                    else
+                        actions[i][SELECT] = 1
+                    end
+                end
+
+                -- Storing the optimal predictions
+                if qValues[i][SKIP] > qValues[i][SELECT] then
+                    actionsOpt[i][SKIP] = 1
+                else
+                    actionsOpt[i][SELECT] = 1
+                end
+                summary = buildSummary(
+                    actions:narrow(1, 1, i), 
+                    sentenceStream:narrow(1, 1, i),
+                    summaryBuffer:narrow(1, i + 1, 1),
+                    use_cuda
+                )
+
+                summaryOpt = buildSummary(
+                    actionsOpt:narrow(1, 1, i), 
+                    sentenceStream:narrow(1, 1, i),
+                    summaryBuffer:narrow(1, i + 1, 1),
+                    use_cuda
+                )
+
+                local recall, prec, f1 = rougeScores(buildTokenCounts(summary), refCounts)
+                local rOpt, pOpt, f1Opt = rougeScores(buildTokenCounts(summaryOpt), refCounts)
+
+                if metric == "f1" then
+                    rouge[i + 1] = threshold(f1, thresh)
+                    rougeOpt[i]  = threshold(f1Opt, thresh)
+                elseif metric == "recall" then
+                    rouge[i + 1] = threshold(recall, thresh)
+                    rougeOpt[i]  = threshold(rOpt, thresh)
+                elseif metric == "precision" then
+                    rouge[i + 1] = threshold(prec, thresh)
+                    rougeOpt[i]  = threshold(pOpt, thresh)
+                end
+
+                if i==streamSize then
+                    rougeRecall = recall
+                    rougePrecision = prec
+                    rougeF1 = f1
+                end
+            end
+
+            local max, argmax = torch.max(qValues, 2)
+            local reward0 = rouge:narrow(1,2, streamSize) - rouge:narrow(1,1, streamSize)
+            local reward_tm1 =  rougeOpt:narrow(1,2, streamSize) - rougeOpt:narrow(1,1, streamSize)
+            local reward = reward0 + gamma * reward_tm1
+            
+            local querySize = query:size(2)
+            local summaryBatch = summaryBuffer:narrow(1, 1, streamSize)
+            local queryBatch = query:view(1, querySize):expand(streamSize, querySize) 
+            --- Storing the data
+            local input = {sentenceStream, queryBatch, summaryBatch}
+            local memory = {input, reward, actions}
+
+            if epoch == 0 and query_id == 1 then
+                fullmemory = memory 
+                randomF1 = f1
+            else
+                -- fullmemory = buildMemory(memory, fullmemory, mem_size, batch_size, use_cuda)
+                fullmemory = buildMemoryOld(memory, fullmemory, mem_size, batch_size, use_cuda)
+            end
+            --- Running backprop
+            -- loss = backPropOld(memory, params, model, criterion, batch_size, mem_size, use_cuda)
+            -- loss = backProp(memory, params, gradParams, optimParams, model, criterion, batch_size, n_backprops, use_cuda)
+            loss = backPropOld(memory, params, gradParams, optimParams, model, criterion, batch_size, memsize, use_cuda)
+
+            if epoch == 0 and query_id == 1 then
+                out = string.format("epoch;epsilon;loss;randomF1;oracleF1;rougeF1;rougeRecall;rougePrecision;actual;pred;nselect;nskip;query\n")
+                perf:write(out)
+            end
+            nactions = torch.totable(actions:sum(1))[1]
+            out = string.format("%i; %.3f; %.6f; %.6f; %.6f; %.6f; %.6f; %.6f; {min=%.3f, max=%.3f}; {min=%.3f, max=%.3f}; %i; %i; %i\n", 
+                epoch, epsilon, loss, randomF1, oracleF1, rougeF1, rougeRecall, rougePrecision,
+                reward:min(), reward:max(),
+                qValues:min(), qValues:max(),
+                nactions[SELECT], nactions[SKIP],
+                query_id
+            )
+            perf:write(out)
+
+            local avpfile = io.open(string.format("plotdata/%s/%i/%i_epoch.txt", nnmod, query_id, epoch), 'w')
+            avpfile:write("predSkip;predSelect;actual;Skip;Select;query\n")
+            for i=1, streamSize do
+                avpfile:write(string.format("%.6f;%.6f;%6f;%i;%i;%i\n", 
+                        qValues[i][SKIP], qValues[i][SELECT], rouge[i], 
+                        actions[i][SKIP], actions[i][SELECT], query_id))
+            end
+            avpfile:close()
+
+            -- Have to zero out a lot of these variables after the fact
+            query_data[query_id] = {
+                sentenceStream,
+                streamSize,
+                query,
+                actions:fill(0),
+                exploreDraws,
+                summaryBuffer:zero(),
+                qValues:zero(),
+                rouge:zero(),
+                actionsOpt:zero(),
+                rougeOpt:zero(),
+                refSummary,
+                refCounts,
+                buffer:zero(),
+                oracleF1
+            }
+        end
+
+        if (epsilon - delta) <= base_explore_rate then
+            epsilon = base_explore_rate
+            if epoch > end_baserate then 
+                base_explore_rate = 0.
+            end
+        else 
+            epsilon = epsilon - delta
+        end
+
+    end
+    print(string.format("Model complete {Selected = %i; Skipped  = %i}; Final Rouge Recall, Precision, F1 = {%.6f;%.6f;%.6f}", 
+                nactions[SELECT], nactions[SKIP], rougeRecall, rougePrecision, rougeF1))
 end
--- Code/testModel.lua
+++ Code/testModel.lua
@@ -36,7 +36,6 @@ nepochs = opt.nepochs
 gamma = opt.gamma
 delta = 1./(opt.nepochs/opt.cuts) 
 base_explore_rate = opt.base_explore_rate
-n_rand = opt.n_rand
 mem_size = opt.mem_size
 batch_size = opt.batch_size
 nnmod = opt.model
@@ -68,8 +67,6 @@ query_fn = input_path .. 'queries_numtext.csv'
 query_file =  csvigo.load({path = query_fn, mode = "large", verbose = false})
 queries = padZeros(buildTermDocumentTable(query_file, nil), 5)
 
-torch.manualSeed(420)
-math.randomseed(420)
 
 pakistan = {
         ['inputs'] = '2012_pakistan_garment_factory_fires_first_sentence_numtext2.csv',
@@ -92,9 +89,9 @@ sandy = {
 
 inputs = {
         aurora, 
-        pakistan,
-        sandy
-    }
+        -- pakistan,
+        -- sandy
+}
 
 if use_cuda then
     Tensor = torch.CudaTensor
@@ -110,174 +107,14 @@ else
 end
 
 -- Initializing the model variables
-vocabSize, query_data = intialize_variables(query_file, inputs, 
-                                            input_path, K_tokens, 
+local vocabSize, query_data = intialize_variables(query_file, inputs, 
+                                            n, input_path, K_tokens, 
                                             maxSummarySize)
 
 local model = buildModel(nnmod, vocabSize, embeddingSize, use_cuda)
-params, gradParams = model:getParameters()
-criterion = nn.MSECriterion()
 
-if use_cuda then
-    criterion = criterion:cuda()
-    model = model:cuda()
-end
+train(inputs, query_data, model, nepochs, nnmod, metric, thresh, 
+      gamma, epsilon, delta, base_explore_rate, end_baserate, 
+      mem_size, batch_size, optimParams, n_backprops, use_cuda)
 
-local perf = io.open(string.format("%s_perf.txt", nnmod), 'w')
-for epoch=0, nepochs do
-    for query_id=1, #inputs do
-        -- Initializing local variables
-            -- in principal I could use the query_data[query_id][1] 
-            -- as the argument but that's much less readable
-        local sentenceStream = query_data[query_id][1]
-        local streamSize = query_data[query_id][2]
-        local query = query_data[query_id][3]
-        local actions = query_data[query_id][4]
-        local exploreDraws = query_data[query_id][5]
-        local summaryBuffer = query_data[query_id][6]
-        local qValues = query_data[query_id][7]
-        local rouge = query_data[query_id][8]
-        local actionsOpt = query_data[query_id][9]
-        local rougeOpt = query_data[query_id][10]
-        local refSummary = query_data[query_id][11]
-        local refCounts = query_data[query_id][12]
-        local buffer = query_data[query_id][13]
-        local oracleF1 = query_data[query_id][14]
-        exploreDraws:uniform(0, 1)
-
-        -- Clearing out the variables from the last time
-
-        for i=1, streamSize do      -- Iterating through individual sentences
-            local sentence = sentenceStream:narrow(1, i, 1)
-            qValues[i]:copy(model:forward({sentence, query, summary}))
-
-            -- epsilon greedy strategy
-            if exploreDraws[i]  <=  epsilon then        
-                actions[i][torch.random(SKIP, SELECT)] = 1
-            else 
-                if qValues[i][SKIP] > qValues[i][SELECT] then
-                    actions[i][SKIP] = 1
-                else
-                    actions[i][SELECT] = 1
-                end
-            end
-
-            -- Storing the optimal predictions
-            if qValues[i][SKIP] > qValues[i][SELECT] then
-                actionsOpt[i][SKIP] = 1
-            else
-                actionsOpt[i][SELECT] = 1
-            end
-            
-            summary = buildSummary(
-                actions:narrow(1, 1, i), 
-                sentenceStream:narrow(1, 1, i),
-                summaryBuffer:narrow(1, i + 1, 1),
-                use_cuda
-            )
-
-            summaryOpt = buildSummary(
-                actionsOpt:narrow(1, 1, i), 
-                sentenceStream:narrow(1, 1, i),
-                summaryBuffer:narrow(1, i + 1, 1),
-                use_cuda
-            )
-
-            local recall, prec, f1 = rougeScores(buildTokenCounts(summary), refCounts)
-            local rOpt, pOpt, f1Opt = rougeScores(buildTokenCounts(summaryOpt), refCounts)
-
-            if metric == "f1" then
-                rouge[i + 1] = threshold(f1, thresh)
-                rougeOpt[i]  = threshold(f1Opt, thresh)
-            elseif metric == "recall" then
-                rouge[i + 1] = threshold(recall, thresh)
-                rougeOpt[i]  = threshold(rOpt, thresh)
-            elseif metric == "precision" then
-                rouge[i + 1] = threshold(prec, thresh)
-                rougeOpt[i]  = threshold(pOpt, thresh)
-            end
-
-            if i==streamSize then
-                rougeRecall = recall
-                rougePrecision = prec
-                rougeF1 = f1
-            end
-        end
-
-        local max, argmax = torch.max(qValues, 2)
-        local reward0 = rouge:narrow(1,2, streamSize) - rouge:narrow(1,1, streamSize)
-        local reward_tm1 =  rougeOpt:narrow(1,2, streamSize) - rougeOpt:narrow(1,1, streamSize)
-        local reward = reward0 + gamma * reward_tm1
-        
-        local querySize = query:size(2)
-        local summaryBatch = summaryBuffer:narrow(1, 1, streamSize)
-        local queryBatch = query:view(1, querySize):expand(streamSize, querySize) 
-        --- Storing the data
-        local input = {sentenceStream, queryBatch, summaryBatch}
-        local memory = {input, reward, actions}
-
-        if epoch == 0 and query_id == 1 then
-            fullmemory = memory 
-            randomF1 = f1
-        else
-            fullmemory = buildMemory(memory, fullmemory, mem_size, batch_size, use_cuda)
-            -- fullmemory = buildMemoryOld(memory, fullmemory, mem_size, batch_size, use_cuda)
-        end
-        --- Running backprop
-        -- loss = backPropOld(memory, params, model, criterion, batch_size, mem_size, use_cuda)
-        loss = backProp(memory, params, gradParams, optimParams, model, criterion, batch_size, n_backprops, use_cuda)
-
-        if epoch == 0 and query_id == 1 then
-            out = string.format("epoch;epsilon;loss;randomF1;oracleF1;rougeF1;rougeRecall;rougePrecision;actual;pred;nselect;nskip;query\n")
-            perf:write(out)
-        end
-        nactions = torch.totable(actions:sum(1))[1]
-        out = string.format("%i; %.3f; %.6f; %.6f; %.6f; %.6f; %.6f; %.6f; {min=%.3f, max=%.3f}; {min=%.3f, max=%.3f}; %i; %i; %i\n", 
-            epoch, epsilon, loss, randomF1, oracleF1, rougeF1, rougeRecall, rougePrecision,
-            reward:min(), reward:max(),
-            qValues:min(), qValues:max(),
-            nactions[SELECT], nactions[SKIP],
-            query_id
-        )
-        perf:write(out)
-
-        local avpfile = io.open(string.format("plotdata/%s/%i/%i_epoch.txt", nnmod, query_id, epoch), 'w')
-        avpfile:write("predSkip;predSelect;actual;Skip;Select;query\n")
-        for i=1, streamSize do
-            avpfile:write(string.format("%.6f;%.6f;%6f;%i;%i;%i\n", 
-                    qValues[i][SKIP], qValues[i][SELECT], rouge[i], 
-                    actions[i][SKIP], actions[i][SELECT], query_id))
-        end
-        avpfile:close()
-
-        query_data[query_id] = {
-            sentenceStream,
-            streamSize,
-            query,
-            actions:fill(0),
-            exploreDraws,
-            summaryBuffer:zero(),
-            qValues:zero(),
-            rouge:zero(),
-            actionsOpt:zero(),
-            rougeOpt:zero(),
-            refSummary,
-            refCounts,
-            buffer:zero(),
-            oracleF1
-        }
-
-    end
-
-    if (epsilon - delta) <= base_explore_rate then
-        epsilon = base_explore_rate
-        if epoch > end_baserate then 
-            base_explore_rate = 0.
-        end
-    else 
-        epsilon = epsilon - delta
-    end
-
-end
-print(string.format("Model complete {Selected = %i; Skipped  = %i}; Final Rouge Recall, Precision, F1 = {%.6f;%.6f;%.6f}", nactions[SELECT], nactions[SKIP], rougeRecall, rougePrecision, rougeF1))
 -- os.execute(string.format("python make_density_gif.py %i %s %s", nepochs, nnmod, metric))
