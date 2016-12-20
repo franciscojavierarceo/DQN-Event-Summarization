1302e63c0924eca0387f3b5fe418b4c2b76570c8
Francisco Arceo <Francisco.Arceo@cba.com.au>
2016-12-20 18:26:32 -0500

    Updated testngrams module

--- Code/testngrams.lua
+++ Code/testngrams.lua
@@ -3,3 +3,8 @@ dofile("Code/utils.lua")
 dofile("Code/utilsNN.lua")
 
 
+genSummary = {3,1,4}
+refSummary = {2,2,3}
+print(genSummary, refSummary)
+recall, prec, f1 = rougeScores(genSummary, refSummary)
+print(string.format("Recall = %.3f; Precision = %.3f; F1 = %.3f", recall, prec, f1))
