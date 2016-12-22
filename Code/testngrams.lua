0a501744f5ddea80f2d03a2a9d3eddb3932d07f1
Francisco Arceo <Francisco.Arceo@cba.com.au>
2016-12-21 20:47:28 -0500

    Updated bigrams, it is working

--- Code/testngrams.lua
+++ Code/testngrams.lua
@@ -3,11 +3,33 @@ dofile("Code/utils.lua")
 dofile("Code/utilsNN.lua")
 
 
-genSummary = {3,1,4}
-refSummary = {2,2,3}
-print(genSummary, refSummary)
+genSummary = {3, 1, 4, 3, 1}
+refSummary = {2, 2, 3, 3, 1}
+-- print(genSummary, refSummary)
 recall, prec, f1 = rougeScores(genSummary, refSummary)
-function ngrams(dic, n)
 
+function ngrams(dic)
+    local out = {}
+    for i=1, #dic do 
+        for j=1, #dic do
+            if i == j then
+                indx = string.format("%i", dic[i])
+                if out[indx] == nil then
+                    out[indx] = 1
+                else 
+                    out[indx]  = out[indx] + 1
+                end
+            else
+                indx = string.format("%i, %i", dic[i], dic[j])
+                if out[indx] == nil then
+                    out[indx] = 1
+                else 
+                    out[indx]  = out[indx] + 1
+                end
+            end
+        end
+    end
+    return out
 end
+print(ngrams(genSummary))
 print(string.format("Recall = %.3f; Precision = %.3f; F1 = %.3f", recall, prec, f1))
