d723d304d917ae75ca2c504003f5065c698b3a89
Francisco Arceo <Francisco.Arceo@cba.com.au>
2016-12-21 20:05:02 -0500

    Updating test grams

--- Code/testngrams.lua
+++ Code/testngrams.lua
@@ -7,4 +7,7 @@ genSummary = {3,1,4}
 refSummary = {2,2,3}
 print(genSummary, refSummary)
 recall, prec, f1 = rougeScores(genSummary, refSummary)
+function ngrams(dic, n)
+
+end
 print(string.format("Recall = %.3f; Precision = %.3f; F1 = %.3f", recall, prec, f1))
