dc67f971d94f631a4b2515339397444dcd81bf8e
Francisco Arceo <Francisco.Arceo@cba.com.au>
2016-12-31 14:54:59 -0500

    Updated simulation...almost have it

--- Code/Simulations/DQN_AdaptiveReg_Simulation.lua
+++ Code/Simulations/DQN_AdaptiveReg_Simulation.lua
@@ -2,53 +2,54 @@ require 'nn'
 math.randomseed(3)
 torch.manualSeed(3)
 -- Building data
-x = torch.rand(20):resize(10, 2)
+x = torch.randn(20):resize(10, 2)
 xhat = x + 0.01 * torch.rand(20):resize(10, 2)
 b = torch.rand(2, 1)
--- np.dot()
-target = torch.round(torch.mm(x, b))
+yhat = torch.mm(x, b)  + torch.randn(10)  -- np.dot()
+n = yhat:size(1)
+onex = torch.ones(n):resize(n, 1)
+target = torch.round(nn.Sigmoid():forward(yhat))
+print(target)
+-- This is the Logistic
+LogisticModel = nn.Sequential():add(nn.Linear(2, 1)):add(nn.LogSoftMax())
 
-model1 = nn.Sequential()
-model1:add(nn.Linear(2, 1))
-model1:add(nn.LogSoftMax())
-
-model2 = nn.Sequential()
-model2:add(nn.Linear(2, 2))
+-- This is the linear regression
+LinearModel = nn.Sequential():add(nn.Linear(2, 2))
 
 --- Concat Table
-model3 = nn.Parallel(2,2)
--- model3 = nn.Concat(2)
-model3:add(model1)
-model3:add(model2)
+-- model3 = nn.Parallel(2,2):add(model1):add(model2)
+FullModel = nn.ConcatTable():add(LinearModel):add(LogisticModel)
+-- model3 = nn.Concat(2):add(model1):add(model2)
 
-model4 = nn.Sequential()
-model4:add(model3)
-model4:add(nn.SplitTable(2))
+-- model4 = nn.Sequential()
+-- model4:add(model3)
+-- model4:add(nn.SplitTable(2))
 
 print("Logistic Pred:")
-pred_log = model1:forward(x)
+pred_log = LogisticModel:forward(x)
 print(pred_log)
 
 print("Linear Pred:")
-pred_reg = model2:forward(x)
+pred_reg = LinearModel:forward(x)
 print(pred_reg)
 
 print("Logistic and Linear Pred:")
-pred_both = model3:forward(x)
-print(pred_both)
-
-print("Joined and separated")
-pred_final = model4:forward(x)
+pred_final = FullModel:forward(x)
 print(pred_final)
 
-nll = nn.CrossEntropyCriterion()
+-- print("Joined and separated")
+-- pred_final = model4:forward(x)
+-- print(pred_final)
+
+nll = nn.BCECriterion()
+-- nll = nn.ClassNLLCriterion()
+-- nll = nn.CrossEntropyCriterion()
 mse = nn.MSECriterion()
 pc = nn.ParallelCriterion():add(nll):add(mse)
 -- Can put in scalar if you want
 -- pc = nn.ParallelCriterion():add(nll, 0.5):add(mse)
 
 print("NLL loss:")
-print(pred_log, target)
 nll_loss = nll:forward(pred_log, target)
 print(nll_loss)
 
@@ -61,5 +62,5 @@ prl_loss = pc:forward( {pred_log, pred_reg} , {target, xhat} )
 print(prl_loss)
 
 print("Parallel Full NLL-MSE loss:")
-prl_final = pc:forward(pred_final, {target, xhat})
+prl_final = pc:forward(pred_final, {xhat, target})
 print(prl_final)
