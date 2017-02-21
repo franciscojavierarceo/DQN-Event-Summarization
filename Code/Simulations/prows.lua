3327b6a1613246e96f66480b1216e3834a296b2d
Francisco Arceo <Francisco.Arceo@cba.com.au>
2017-02-21 04:42:24 +0400

    have a way to parallelize selection of rows

--- Code/Simulations/prows.lua
+++ Code/Simulations/prows.lua
@@ -4,24 +4,33 @@ require 'rnn'
 require 'optim'
 require 'parallel'
 
+
+nforks = 10
 function worker()
     require 'sys'
     require 'torch'
+    require 'math'
+    nforks = 10
     while true do
         m = parallel.yield() -- yield = allow parent to terminate me
         if m == 'break' then 
             break 
         end
         local t = parallel.parent:receive()  -- receive data
-        print(t.data:size(1) % parallel.id)
+        -- print(math.floor(4/10))
+        -- print(t.data:size(1) / nforks, parallel.id, t.data:size(1))
+        chunksize = math.floor(t.data:size(1) / nforks)
+        start_index = chunksize * (parallel.id-1) + 1
+        end_index = chunksize * parallel.id
+        -- print(parallel.id, start_index, end_index, t.data[{start_index, end_index}])
+        print(parallel.id, start_index, end_index, t.data[{{start_index, end_index}}]:size(1))
+        -- xs[{{from, to}}]
         parallel.parent:send(t.data)
-
     end
 end
 
-
 function parent(input)
-    parallel.nfork(10)
+    parallel.nfork(nforks)
     parallel.children:exec(worker)
     send_var = {name='my variable', data=input} 
 
