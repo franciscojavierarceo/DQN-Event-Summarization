91fd2656d14e334847a6e505458a0642c5088761
Francisco Arceo <Francisco.Arceo@cba.com.au>
2017-02-21 08:11:03 +0400

    updated code...seems to be working nicely

--- Code/Simulations/prows.lua
+++ Code/Simulations/prows.lua
@@ -4,8 +4,6 @@ require 'rnn'
 require 'optim'
 require 'parallel'
 
-nforks = 10
-
 function genNbyK(n, k, a, b)
     out = torch.LongTensor(n, k)
     for i=1, n do
@@ -16,6 +14,62 @@ function genNbyK(n, k, a, b)
     return out
 end
 
+function buildTokenCounts(summary, stopwordlist)
+    local counts = {}
+    if #summary:size() == 1 then
+        summary = summary:resize(1, summary:size(1))
+    end
+    for i=1, summary:size(1) do
+        for j=1, summary:size(2) do
+            if summary[i][j] > 0 then
+                local token = summary[i][j]
+                if counts[token] == nil then
+                    counts[token] = 1
+                else
+                    counts[token] = counts[token] + 1
+                end
+            end
+        end
+    end
+    return counts
+end
+function rougeScores(genSummary, refSummary)
+    local genTotal = 0
+    local refTotal = 0
+    local intersection = 0
+    -- Inserting the missing keys
+    for k, genCount in pairs(genSummary) do
+        if refSummary[k] == nil then
+            refSummary[k] = 0
+        end
+    end
+    for k, refCount in pairs(refSummary) do
+        local genCount = genSummary[k]
+        if genCount == nil then 
+            genCount = 0 
+        end
+        intersection = intersection + math.min(refCount, genCount)
+        refTotal = refTotal + refCount
+        genTotal = genTotal + genCount
+    end
+    recall = intersection / refTotal
+    prec = intersection / genTotal
+    if refTotal == 0 then
+        recall = 0
+    end 
+    if genTotal == 0 then
+        prec = 0
+    end
+    -- tmp = {intersection, refTotal, genTotal}
+    if recall > 0 or prec > 0 then
+        f1 = (2 * recall * prec) / (recall + prec)
+    else 
+        f1 = 0
+    end
+    -- return recall, prec, f1
+    return f1
+end
+
 function worker()
     require 'sys'
     require 'torch'
@@ -23,6 +77,9 @@ function worker()
 
     function buildTokenCounts(summary, stopwordlist)
         local counts = {}
+        if #summary:size() == 1 then
+            summary = summary:resize(1, summary:size(1))
+        end
         for i=1, summary:size(1) do
             for j=1, summary:size(2) do
                 if summary[i][j] > 0 then
@@ -74,7 +131,7 @@ function worker()
         return recall, prec, f1
     end
 
-    nforks = 10
+    nforks = 10 
 
     while true do
         m = parallel.yield() -- yield = allow parent to terminate me
@@ -82,6 +139,7 @@ function worker()
             break 
         end
         input = parallel.parent:receive()  -- receive data
+        -- nforks = input.data[3]
         chunksize = math.floor(input.data[1]:size(1) / nforks)
         start_index = chunksize * (parallel.id-1) + 1
         end_index = chunksize * parallel.id
@@ -99,7 +157,7 @@ function worker()
 end
 
 function parent(input)
-    parallel.nfork(nforks)
+    parallel.nfork(input[3])
     parallel.children:exec(worker)
     send_var = {name='my variable', data=input} 
 
@@ -107,17 +165,37 @@ function parent(input)
     parallel.children:send(send_var)
     replies = parallel.children:receive()
     parallel.children:join('break')
-    print(replies)
+    -- Returns the data in order of the index
+    -- print(replies)
+    -- print(torch.Tensor(replies))
 end
 
+n = 10
+k = 5
+a = 0
+b = 100
 
-xs = genNbyK(10, 5, 0, 5)
-truexs = genNbyK(10, 5, 0, 5)
+xs = genNbyK(n, k, a, b)
+truexs = genNbyK(n, k, a, b)
 
--- protected execution
-ok, err = pcall(parent, {xs, truexs})
+nClock = os.clock()
+ok, err = pcall(parent, {xs, truexs, n})
+print(string.format("Parallel Elapsed time: %.5f" % (os.clock()-nClock) ))
+
 if not ok then 
     print(err) 
 end
 
 parallel.close()
+
+nClock = os.clock()
+scores = torch.zeros(n)
+for i=1, n do
+    perf = rougeScores(
+                buildTokenCounts(xs[i]), 
+                buildTokenCounts(truexs[i])
+            )
+    scores[i] = perf
+end
+-- print(scores)
+print(string.format("Linear Elapsed time: %.5f" % (os.clock()-nClock) ))
