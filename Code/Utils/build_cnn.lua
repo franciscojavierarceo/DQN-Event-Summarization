1abbcd885fc950638b549ac19a9d3f675045832b
Francisco Javier Arceo <arceofrancisco@gmail.com>
2017-05-04 18:29:32 -0400

    Uploading changes to lua file

--- Code/Utils/build_cnn.lua
+++ Code/Utils/build_cnn.lua
@@ -1,4 +1,5 @@
 require 'csvigo'
+require 'math' 
 
 input_path = '/home/francisco/GitHub/DQN-Event-Summarization/data/cnn_tokenized/'
 inputfile ='cnn_data_sentence_00.csv'
@@ -8,15 +9,31 @@ local qtokens = {}
 local stokens = {}
 local tstokens = {}
 
+
+function makeInt(x)
+    --- Casts string values into integers when reading in the data
+    local out = {}
+    for k,v in pairs(x) do
+        table.insert(out, tonumber(v))
+    end
+    return out 
+end
+
+
+maxq = 0
+maxs = 0
+maxts = 0
+
 for i, row in pairs(mydata) do
 	if i > 1 and row ~= nil then
-		qtokens[i-1] = row[1]:split(" ")
-		stokens[i-1] = row[2]:split(" ")
-		tstokens[i-1] = row[3]:split(" ")
-		--qtokens[i-1] = row[1]
-		--stokens[i-1] = row[2]
-		--tstokens[i-1] = row[3]
+		qtokens[i-1]  = makeInt(row[1]:split(" "))
+		stokens[i-1]  = makeInt(row[2]:split(" "))
+		tstokens[i-1] = makeInt(row[3]:split(" "))
+		maxq = math.max(maxq, #qtokens[i-1])
+		maxs = math.max(maxs, #stokens[i-1])
+		maxts = math.max(maxts, #tstokens[i-1])
 	end
 end
 
-print(stokens[1])
+print(stokens[{{1,4}}])
+print(maxq, maxs, maxts)
