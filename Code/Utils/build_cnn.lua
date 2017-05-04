f836fa789b04aee258b8a46ef5e075818e55909f
Francisco Arceo <Francisco.Arceo@cba.com.au>
2017-05-04 19:39:42 -0400

    made loading and extraction of data into function

--- Code/Utils/build_cnn.lua
+++ Code/Utils/build_cnn.lua
@@ -3,33 +3,39 @@ require 'csvigo'
 dofile("Code/utils.lua")
 dofile("Code/utilsNN.lua")
 
-input_path = '/home/francisco/GitHub/DQN-Event-Summarization/data/cnn_tokenized/'
-inputfile ='cnn_data_sentence_00.csv'
-mydata =  csvigo.load({path = input_path .. inputfile, mode = "large", verbose = false})
 
-local qtokens = {}
-local stokens = {}
-local tstokens = {}
+function readCNN(input_path, inputfile, idx)
+    inputfile = string.format('cnn_data_sentence_%02d.csv', idx)
 
-maxq  = 0
-maxs  = 0
-maxts = 0 
+    mydata =  csvigo.load({path = input_path .. inputfile .. '.csv', mode = "large", verbose = false})
+    local qtokens, stokens, tstokens = {}, {}, {}
+    local maxq, maxs, maxts = 0, 0, 0
 
-for i, row in pairs(mydata) do
-    if i > 1 and row ~= nil then
-        qtokens[i-1] = row[1]:split(" ")
-        stokens[i-1] = row[2]:split(" ")
-        tstokens[i-1] = row[3]:split(" ")
-        maxq = math.max(maxq, #qtokens[i-1])
-        maxs = math.max(maxs, #stokens[i-1])
-        maxts = math.max(maxts, #tstokens[i-1])
+    for i, row in pairs(mydata) do
+        if i > 1 and row ~= nil then
+            qtokens[i-1] = row[1]:split(" ")
+            stokens[i-1] = row[2]:split(" ")
+            tstokens[i-1] = row[3]:split(" ")
+            maxq = math.max(maxq, #qtokens[i-1])
+            maxs = math.max(maxs, #stokens[i-1])
+            maxts = math.max(maxts, #tstokens[i-1])
+        end
     end
+
+    q_x = torch.Tensor(padZeros(qtokens, maxq))
+    s_x = torch.Tensor(padZeros(stokens, maxs))
+    ts_x = torch.Tensor(padZeros(tstokens, maxts))
+
+    qfile = string.format("query_tokens_sid_%i.dat", idx)
+    sfile = string.format("sentence_tokens_sid_%i.dat", idx)
+    tsfile = string.format("truesummary_tokens_sid_%i.dat", idx)
+
+    torch.save(outputpath .. qfile, q_x)
+    torch.save(outputpath .. sfile, s_x)
+    torch.save(outputpath .. tsfile, ts_x)
 end
 
-print(maxq, maxs, maxts)
-q_x = torch.Tensor(padZeros(qtokens, maxq))
--- s_x = torch.Tensor(padZeros(stokens, maxq))
--- ps_x = torch.Tensor(padZeros(tstokens, maxq))
+input_path = '/home/francisco/GitHub/DQN-Event-Summarization/data/cnn_tokenized/'
+outputpath = "/home/francisco/GitHub/DQN-Event-Summarization/data/training/"
 
-torch.save("query_tokens.dat", q_x)
-print("data saved to query_tokens.dat")
+readCNN(input_path, inputfile, 0)
