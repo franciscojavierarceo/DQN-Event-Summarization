c0e9b3582c5d5b386a80faa031542130acd43d49
Francisco Javier Arceo <arceofrancisco@gmail.com>
2017-05-05 08:23:31 -0400

    finished file to load cnn data

--- Code/Utils/load_cnn.lua
+++ Code/Utils/load_cnn.lua
@@ -3,13 +3,14 @@ require 'csvigo'
 dofile("Code/utils.lua")
 
 
-outputpath = '/home/francisco/GitHub/DQN-Event-Summarization/data/cnn_tokenized/'
+outputpath = '/home/francisco/GitHub/DQN-Event-Summarization/data/training/'
 
 
 queries, sentences, trueSummary  = {}, {}, {}
+
 for i = 0, 124 do
 	queries[i + 1 ] = torch.load(outputpath .. string.format("qtokens_sid_%i.dat", i))
 	sentences[i + 1] = torch.load(outputpath .. string.format("stokens_sid_%i.dat", i))
 	trueSummary[i + 1] = torch.load(outputpath .. string.format("tstokens_sid_%i.dat", i))
 end
-
+print("Data loaded")
