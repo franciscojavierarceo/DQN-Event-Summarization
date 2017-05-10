08c46c3de7b8250d808bd5d50b8fa5dd96c1b818
Francisco Javier Arceo <arceofrancisco@gmail.com>
2017-05-10 12:55:07 -0400

    updated batch script

--- Code/batchDQN.lua
+++ Code/batchDQN.lua
@@ -12,19 +12,21 @@ require 'csvigo'
 dl = require 'dataload'
 
 dofile("Code/Utils/load_cnn.lua")
+dofile("Code/utilsNN.lua")
 
 outputpath = '/home/francisco/GitHub/DQN-Event-Summarization/data/training/'
 queries, sentences, trueSummaries = loadCNN(outputpath)
 vocabSize = 20001
 embDim = 500
 
-n = queries:size(1)
-n_s = sentences:size(2)
+n = queries[1]:size(1)
+n_s = sentences[1]:size(2)
 k = 10
 
 totalPredsummary = LongTensor(n, n_s * k):fill(0)
 
 model = buildModel('bow', vocabSize, embDim, 'f1', false, false)
 
+print(#queries[1]), #sentences[1], #totalPredsummary)
 preds = model:forward(queries[1], sentences[1], totalPredsummary ) 
 print(preds:size())
