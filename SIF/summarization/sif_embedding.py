07e808b3be008892b11a21197924f91d5fb02601
Francisco Javier Arceo <arceofrancisco@gmail.com>
2017-11-15 22:32:34 -0500

    updated sif embedding with explicit path to gloe dataset

--- SIF/summarization/sif_embedding.py
+++ SIF/summarization/sif_embedding.py
@@ -4,7 +4,7 @@ import data_io, params, SIF_embedding
 import show_chunked
 
 # input
-wordfile = '../data/glove.840B.300d.txt' # word vector file, can be downloaded from GloVe website
+wordfile = '/home/francisco/GitHub/SIF/data/glove.840B.300d.txt'   # word vector file, can be downloaded from GloVe website
 weightfile = '../auxiliary_data/enwiki_vocab_min200.txt' # each line is a word and its frequency
 weightpara = 1e-3 # the parameter in the SIF weighting scheme, usually in the range [3e-5, 3e-3]
 rmpc = 1 # number of principal components to remove in SIF weighting scheme
---
+++ SIF/summarization/stanfordexample.sh
@@ -0,0 +1,1 @@
+echo "My 1st sentence. Does it work for questions? My third sentence." | java edu.stanford.nlp.process.DocumentPreprocessor
