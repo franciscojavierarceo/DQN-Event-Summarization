510df3b10bda8a06cabad68361e62e8864240ab4
Francisco Javier Arceo <arceofrancisco@gmail.com>
2017-11-15 22:14:09 -0500

    updated notebook and tensoring of sentences--irrelevant at this point--and added the SIF sentence embedding

--- Code/tensor_sentences.py
+++ Code/tensor_sentences.py
@@ -18,51 +18,53 @@ import torch.optim as optim
 from joblib import Parallel, delayed
 
 def make_bow_vector(sentence, word_to_ix):
-    vec = torch.zeros(len(word_to_ix))
-    for token in sentence:
-        vec[token] += 1
-    return vec.view(1, -1)
+        vec = torch.zeros(len(word_to_ix))
+        for token in sentence:
+                vec[token] += 1
+        return vec.view(1, -1)
 
 def main():
-	def make_bow_vector(sentence, word_to_ix): 
-		vec = torch.zeros(len(word_to_ix))
-		for token in sentence:
-			vec[token] += 1
-		return vec.view(1, -1)
-	
-	inputfile = "/home/francisco/GitHub/DQN-Event-Summarization/data/cnn_tokenized/cnn_data_corpus.csv"
-	inputdict = "/home/francisco/GitHub/DQN-Event-Summarization/data/cnn_tokenized/cnn_total_corpus_smry.csv"
-	
-	qdf = pd.read_csv(inputfile)
-	qdict = pd.read_csv(inputdict)
+    def make_bow_vector(sentence, word_to_ix): 
+        vec = torch.zeros(len(word_to_ix))
+        for token in sentence:
+            vec[token] += 1
+        return vec.view(1, -1)
+    
+    inputfile = "/home/francisco/GitHub/DQN-Event-Summarization/data/cnn_tokenized/cnn_data_corpus.csv"
+    inputdict = "/home/francisco/GitHub/DQN-Event-Summarization/data/cnn_tokenized/cnn_total_corpus_smry.csv"
+    
+    qdf = pd.read_csv(inputfile, nrows=2000)
+    qdict = pd.read_csv(inputdict)
 
-	queries = qdf['query_id']
-	sentences = qdf[[x for x in qdf.columns if 'stokens_' in x]]
+    queries = qdf['query_id']
+    sentences = qdf[[x for x in qdf.columns if 'stokens_' in x]]
 
-	true_summaries = {}
-	for queryid, true_summary in zip(queries, qdf['tstokens']):
-		true_summaries[queryid] = Counter([int(x) for x in true_summary.split(" ")])
-	
-	corpus_dict = dict(zip(qdict['id'].values, qdict['token'].values))
-	
-	sentence_list = []
-	
-	def tensorSentence(sentencevar):
-		xs = torch.zeros(sentences.shape[0], len(corpus_dict))	
-		for i, row in enumerate(sentences[sentencevar]):
-			if type(row) == str:
-				tokens = row.split(" ")
-			else:
-				tokens = [0]
-			
-			if len(tokens) > 0:
-				xs[i, :] = make_bow_vector([int(s) for s in tokens], corpus_dict)
-		return xs
-	
-	sentence_list.append(tensorSentence('stokens_0'))
-	sentence_list.append(tensorSentence('stokens_1'))
-	
-	print(len(sentence_list))
-	
+    true_summaries = {}
+    for queryid, true_summary in zip(queries, qdf['tstokens']):
+        true_summaries[queryid] = Counter([int(x) for x in true_summary.split(" ")])
+    
+    corpus_dict = dict(zip(qdict['id'].values, qdict['token'].values))
+    
+    
+    def tensorSentence(sentencevar):
+        xs = torch.zeros(sentences.shape[0], len(corpus_dict))    
+        for i, row in enumerate(sentences[sentencevar]):
+            if type(row) == str:
+                tokens = row.split(" ")
+            else:
+                tokens = [0]
+            
+            if len(tokens) > 0:
+                xs[i, :] = make_bow_vector([int(s) for s in tokens], corpus_dict)
+        return xs
+    
+        
+    sentence_list = []
+
+    for sentence_index in sentences.columns:
+        sentence_list.append(tensorSentence(sentence_index))
+
+    torch.save(sentence_list, "/home/francisco/GitHub/DQN-Event-Summarization/data/cnn_tokenized/cnn_tensors.pkl")
+
 if __name__ == '__main__':
-	main()
+    main()
---
+++ SIF/summarization/show_chunked.py
@@ -0,0 +1,31 @@
+import glob
+import os
+import struct
+from nltk.tokenize import sent_tokenize
+
+from tensorflow.core.example import example_pb2
+
+def getsentences(file0):
+    reader = open(file0, 'rb')
+    len_bytes = reader.read(8)
+    str_len = struct.unpack('q', len_bytes)[0]
+    example_str = struct.unpack("%ds" % str_len, reader.read(str_len))[0]
+    tmp = example_pb2.Example.FromString(example_str)
+
+    article  = str(tmp.features.feature['article'].bytes_list.value[0])
+    abstract = str(tmp.features.feature['abstract'].bytes_list.value[0])
+
+    # print(len(sent_tokenize(article)))
+    # print(sent_tokenize(article))
+
+    # print(len(sent_tokenize(abstract)))
+    # print(sent_tokenize(abstract))
+
+    return sent_tokenize(abstract), sent_tokenize(article)
+
+def main():
+    file0 = '/home/francisco/GitHub/cnn-dailymail/finished_files/chunked/train_000.bin'
+    return getsentences(file0)
+
+if __name__ == '__main__':
+        print(main())
